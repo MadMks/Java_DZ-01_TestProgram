f1ac07b25edee3238f887f8decd18833dcbeaebf
Makc <mks59k@gmail.com>
2019-01-19 19:04:28 +0200

    Реализовано перечисление

---
+++ src/Level/Difficulty.java
@@ -0,0 +1,14 @@
+package Level;
+
+public enum Difficulty {
+    Easy(1, "easy"),
+    Hard(2,"hard");
+
+    private int code;
+    private String title;
+
+    Difficulty(int code, String title){
+        this.code = code;
+        this.title = title;
+    }
+}
